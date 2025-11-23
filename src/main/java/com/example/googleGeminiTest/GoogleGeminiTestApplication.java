eb15516818ca8e6cec136a8e54e2074ab2ad47da
Wander Madeira da Silva <wandermadeirasilva2@gmail.com>
2025-11-23 12:17:24 +0000

    just draft

---
+++ src/main/java/com/example/googleGeminiTest/GoogleGeminiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.googleGeminiTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleGeminiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleGeminiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/googleGeminiTest/GoogleGeminiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googleGeminiTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleGeminiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
